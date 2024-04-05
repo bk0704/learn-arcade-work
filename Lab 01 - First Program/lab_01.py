ad506c602d3a0019c1b2bf73f02e7af04755873e
bk0704 <bilal.k1@icloud.com>
2024-04-05 14:33:56 -0400

    changed lab 1

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,5 @@
+print("Once upon a time there were 3 little piggies")
+print("But then one of them said \"you G there is a wolf coming\"")
+print("""And the second pig said
+\"yo homie this is one of out opps\"""")
+print("and the third piggie said, \n\"yeah bro he's an OG blood\"")
